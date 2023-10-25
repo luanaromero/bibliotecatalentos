18742c7eac251d6ec4892a5d0c87f3e0b5533051
luanaromero <ingridluanar@gmail.com>
2023-10-25 20:15:06 -0300

    cambio de nombre de ruta para eliminar con ISBN

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,6 @@ export const router = Router()
 
 router.get('/libros' , libro.getAll);
 router.post('/libro', libro.add);
-router.delete('/libro', libro.delete);
+router.delete('/libro', libro.deleteISBN);
 router.put('/libro', libro.update);
 router.get('/libro' , libro.getOne);
